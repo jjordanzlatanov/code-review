a4d5246fcc485d3e45f65a9eff18f975b10f4a76
jjordanzlatanov <55088280+jjordanzlatanov@users.noreply.github.com>
2021-10-21 14:31:45 +0300

    Revert "Code review - yosif code"

--- yosif/mysite/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from api import views
-
-urlpatterns = [
-    path('cars', views.cars_json),
-    path('board', views.board_json),
-    path('table', views.table_json),
-]
